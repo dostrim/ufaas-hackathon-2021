a505d7857d5ca58a73e5ea129b1b5e1b9b56fd22
Matovu Joseph <m.joseph550077@gmail.com>
2021-10-26 17:06:46 +0300

    updates

---
+++ app/src/main/java/com/afrosoft/csadatacenter/ui/home/bestPractices/BestPracticesActivity.kt
@@ -0,0 +1,12 @@
+package com.afrosoft.csadatacenter.ui.home.bestPractices
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.afrosoft.csadatacenter.R
+
+class BestPracticesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_best_practices)
+    }
+}
